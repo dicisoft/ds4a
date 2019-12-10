afd640de23f5ebd33a3a4e27771e131c68bee9b7
Julian Gonzalez <jgonzalez@identidadtelecom.net>
2019-12-09 19:52:06 -0500

    New structure CSS
    New structure CSS

--- layouts.py
+++ layouts.py
@@ -28,7 +28,7 @@ dashboard = html.Div(
                     ],
                     
                  
-                        style={"background-color": "#FFFFFF", "height": "70px", "position": "fixed" , "z-index": "99999" },
+                        style={"background-color": "#FFFFFF", "height": "70px", "position": "fixed" , "z-index": "99999" , "top": "0" },
                 ),
                 dbc.Col(
                     [
@@ -37,9 +37,6 @@ dashboard = html.Div(
 
                                 html.H6("Map"),
                                 html.Br(),
-                                html.Button('Documentation', className='button-dash'),
-                                html.Br(),
-                                html.Br(),
                                 dcc.Dropdown(id='select-airport',
                                 options=[{'label':label,'value':val} for label, val in zip(df['station_name'].unique(),df['station'].unique())],
                                 value='SKBO'),
@@ -47,31 +44,27 @@ dashboard = html.Div(
                                 html.Div(id='map-div')                                
                             ],     
                             md=12),
-                            dbc.Col([
-                                html.H6("Grid"),
-                                dt.DataTable(
-                                    id='table',
-                                    columns=vars_list_dt,
-                                    data=df.to_dict('records'),
-                                    style_cell={'width': '50px'},
-                                    style_table={
-                                            'maxHeight': '450px',
-                                            'overflowY': 'scroll'
-                                        }
-                                )
+                                dbc.Col([
+                                html.H6("Horizontal visibility Chart"),
+                                dcc.Graph(id="horizontal-vis-plot"),
+                                html.Br(),
                             ],
-                            md=12)
+                            md=12),
                     ],
                     md=6,
-                    style={"background-color": "#F4F4F4", "height": "100vh", "padding": "0.% 0",  "overflow": "scroll" ,  "top": "70px"},
+                    style={"background-color": "#F4F4F4", "height": "100%", "padding": "0", "top": "70px"},
                 ),
                  dbc.Col(
                     [
                             dbc.Col([
                                 html.H6("Visibility Chart"),
                                 html.Br(),
+                                html.Button('Documentation', className='button-dash'),
+                                html.Br(),
+                                html.Br(),
                                 html.Button('Proyection', id='btnProyection', className='button-dash'),
                                 html.Br(),
+                                
                                      html.H6("Vertical visibility"),
                                 dcc.Checklist(id='checklist-vertical',
                                         options=[
@@ -100,39 +93,55 @@ dashboard = html.Div(
                                     labelStyle={'display': 'inline-block'}
                                        
                                 ),
-                            html.Br(), 
-                             html.Br(),         
+                            html.Br(),
+                            html.Br(),         
+ 
+                              
                             ],
                             md=12), 
                             
                             dbc.Col([
                                 html.H6("Vertical visibility Chart"),
                                 dcc.Graph(id="vertical-vis-plot"),
+                                html.Br(),
                             ],
                             md=12),                                         
-                            dbc.Col([
-                                html.H6("Horizontal visibility Chart"),
-                                dcc.Graph(id="horizontal-vis-plot"),
+                        
+                       
+                            
+                    ],       
+                    md=6,
+                    style={"background-color": "#F4F4F4", "height": "100%", "padding": "0",  "top": "70px", "height": "100%" },
+                ),        
+            ]  
+        ),
+         dbc.Col([
+                                html.H6("Grid"),
+                                dt.DataTable(
+                                    id='table',
+                                    columns=vars_list_dt,
+                                    data=df.to_dict('records'),
+                                    style_cell={'width': '50px'},
+                                    style_table={
+                                            'maxHeight': '450px',
+                                            'overflowY': 'scroll'
+                                        }
+                                )
                             ],
                             md=12),
-                            dbc.Col([
-                                                                     html.Br(),
+                                 dbc.Col([
+                                 html.Br(),
                                  html.Img(src='../static/images/logo-dici.png'),
                                   html.Img(src='../static/images/logo-mintic.png'),
                                   html.Img(src='../static/images/logo-c-o.png'),
                                   html.Img(src='../static/images/logo-softbank.png'),
-                                                                       html.Br(),
-                                                                        html.Br(),
+                                  html.Br(),
+                                  html.Br(),
                             ],
-                            md=12),
-                            
-                    ],       
-                    md=6,
-                    style={"background-color": "#F4F4F4", "height": "100vh", "padding": "0.% 0",  "overflow": "scroll" ,  "top": "70px"},
-                ),        
-            ]  
-        )   
+                             ),
+          
     ],
+    
     className="mt-4",
       style={"background-color": "#F4F4F4", "height": "100%", "padding": "0.5%"},
     )
